8a03fd7959d176d9e46d67e3b419a34fd76b879c
David Taylor <tinystatemachine@gmail.com>
2016-02-05 18:29:59 +0000

    rename sample project to roachspeed

--- roachspeed/settings.py
+++ roachspeed/settings.py
@@ -63,7 +63,7 @@ if DEBUG:
                                  (request.build_absolute_uri(),
                                   traceback.format_exc()))
     # And add it to the middleware classes
-    MIDDLEWARE_CLASSES += ('sample_project.settings.LogUncatchedErrors',)
+    MIDDLEWARE_CLASSES += ('roachspeed.settings.LogUncatchedErrors',)
 
     # set shown level of logging output to debug
     logging.basicConfig(level=logging.DEBUG)
